2a8cef50db92f05120eaff0e3e4633fc589c1925
tkfm4211 <ne.takafumimi@gmail.com>
2023-03-19 13:36:43 +0900

    abc

---
+++ 済/01_JQuery/JQuery　課題/1.js
@@ -0,0 +1,11 @@
+$(function(){
+  $(".abc h1").css("color","red");
+  });
+
+$(function(){
+    $(".abc p").fadeOut("fast");
+});
+
+$(function(){
+    $(".abc h2").text("ddd");
+});
---
+++ 済/01_JavaScript/javascript　課題/1.js
@@ -0,0 +1,112 @@
+for(var a=1;a<=5;a++){
+    document.write("★");
+}
+
+document.write("<br><br>");
+
+for(var b=1;b<=7;b++){
+    if(b==4){
+        document.write("<br>");
+    }else{
+        document.write("★");
+    }
+}
+
+document.write("<br><br>");
+
+
+//
+//for(var b=1;b<=12;b++){
+//    document.write("★");
+//    if(b%4==0){
+//        document.write("<br>");
+//    }
+//}
+//
+//document.write("<br><br>");
+
+
+
+
+for(c=10;c>=0;c--){
+    if(c%6==5){
+        document.write("<br>");
+    }else{
+        document.write("☆");
+    }
+}
+
+document.write("<br><br>");
+
+for(d=1;d<=4;d++){
+    for(e=1;e<=5;e++){
+        document.write("★");
+    }
+    document.write("<br>");
+}
+
+document.write("<br><br>");
+
+for(f=1;f<=16;f++){
+    if(f%4==0){
+        document.write("<br>");
+    }else{
+        document.write("★");
+    }
+}
+
+document.write("<br><br>");
+
+for(g=1;g<=12;g++){
+    if(g%4==0){
+        document.write("<br>");
+    }else if(g%2==0){
+        document.write("☆");
+    }else{
+        document.write("★");
+    }
+    }
+
+document.write("<br><br>");
+
+for(h=1;h<=24;h++){
+    if(h%6==0){
+        document.write("<br>");
+    }else if(h%2==0){
+        document.write("☆");
+    }else{
+        document.write("★");
+    }
+}
+
+document.write("<br><br>");
+
+
+
+//
+//for(h=1;h<=20;h++){
+//    if(h%2==0){
+//        document.write("☆");
+//    }else{
+//        document.write("★");
+//    }
+//    if(h%5==0){
+//        document.write("<br>");
+//    }
+//}
+//
+//document.write("<br><br>");
+
+
+
+
+
+
+for(i=0;i<5;i++){
+    for(j=0;j<=i;j++){
+        document.write("★");
+    }
+    document.write("<br>");
+}
+
+
